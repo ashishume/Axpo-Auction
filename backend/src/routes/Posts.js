cefd5e63d1d7ff681cdf31dfaabd6f59680b5179
Ashish Debnath <ashishume@gmail.com>
2024-06-07 20:24:01 +0530

    feat: posts deleted

--- backend/src/routes/Posts.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authenticateToken = require("../controllers/authMiddleware");
-const pool = require("../controllers/db-connect");
-
-// Create and save a new product
-router.post("/product", async (req, res) => {});
-
-module.exports = router;
