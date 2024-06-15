fa4165cfa5e84fcb7c99cb94ae165f5d4bfa8597
Ashish Debnath <ashish.debnath@soroco.com>
2024-06-16 00:05:06 +0530

    feat: updated

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const createTables = require("./src/utils/createTables");
-const dotenv = require("dotenv");
+const dotenv = require('dotenv'); 
 require("./src/controllers/db-connect");
 app.use(cookieParser());
 dotenv.config(); // Load environment variables from .env file
@@ -15,13 +15,14 @@ dotenv.config(); // Load environment variables from .env file
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  "*",
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow requests from all origins
+    callback(null, true);
+  },
+  credentials: true,
+}));
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to Axpo auction");
