19c9bc5cee695a0818d3b25ce167c6692d576e37
Ashish Debnath <ashishume@gmail.com>
2024-06-20 22:12:37 +0530

    feat: docker containers

--- backend/routes.js
+++ backend/routes.js
@@ -4,7 +4,7 @@ const app = express();
 const userRoutes = require("./src/routes/Auth");
 const productRoutes = require("./src/routes/Products");
 const bidProducts = require("./src/routes/BidProducts");
-const seats = require("./src/routes/auctions");
+const seats = require("./src/routes/Auctions");
 // const allowCors = require("./src/utils/allowCors");
 
 [userRoutes, productRoutes, bidProducts, seats].forEach((apiRoutes) =>
